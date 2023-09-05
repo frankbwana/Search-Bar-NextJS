b00476853a1a2c9e04dc812bb9195db7998d5297
Franklin Bwana <bwanafranklin@gmail.com>
2023-09-05 23:09:19 +0300

    First commit

---
+++ app/api/movies/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+
+async function fetchMovie() {
+    const response = await fetch('https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0', {
+        "method": "GET",
+        "headers": {
+            'X-RapidAPI-Key': '19a849e823msh944a30c9ab30267p187106jsn0360bd1b0065',
+		'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+        }
+       
+    })
+
+    const movies = await response.json();
+    return movies;
+}
+
+export async function GET(request)  {
+    const movies = await fetchMovie();
+    return NextResponse.json(movies);
+}
